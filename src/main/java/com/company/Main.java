8aa7167e8131618117501182027831f5434bfb55
Paul Zharskiy <paulzharskiy@gmail.com>
2018-11-01 09:53:11 +0300

    Maven

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import com.company.entities.Catalog;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Catalog catalog = new Catalog("d:/catalogtest2/");
+        catalog.printToFile();
+         catalog.print();
+       catalog.findDublicates();
+        catalog.findDublicatesWithoutCheckSum();
+    }
+}
+
